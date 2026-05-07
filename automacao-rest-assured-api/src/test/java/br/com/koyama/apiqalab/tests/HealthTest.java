941b4841201c3cd997618aa6ff177be3f11e167a
Matheus Koyama <koyamaCRS@gmail.com>
2026-05-07 14:38:24 -0300

    feat: adiciona teste do endpoint health

--- automacao-rest-assured-api/src/test/java/br/com/koyama/apiqalab/tests/HealthTest.java
+++ automacao-rest-assured-api/src/test/java/br/com/koyama/apiqalab/tests/HealthTest.java
@@ -1,8 +1,24 @@
 package br.com.koyama.apiqalab.tests;
 
+import org.junit.jupiter.api.Test;
+
 import br.com.koyama.apiqalab.base.BaseTest;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class HealthTest extends BaseTest {
 
-
+	   @Test
+	    void deveRetornarApiDisponivel() {
+	       given()
+	        .when()
+	             .get("/health")
+	        .then()
+	             .statusCode(200)
+	             .body("success", equalTo(true))
+                 .body("data.status", equalTo("UP"))
+	             ;
+	    }
 }
