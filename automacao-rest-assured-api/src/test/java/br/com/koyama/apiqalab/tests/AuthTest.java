e94d8f41d0d65ad6ae55e686b5f8befe6e0ea390
Matheus Koyama <koyamaCRS@gmail.com>
2026-05-08 12:41:44 -0300

    test: implementa testes de health, auth, reset e clientes

--- automacao-rest-assured-api/src/test/java/br/com/koyama/apiqalab/tests/AuthTest.java
+++ automacao-rest-assured-api/src/test/java/br/com/koyama/apiqalab/tests/AuthTest.java
@@ -1,11 +1,80 @@
 package br.com.koyama.apiqalab.tests;
 
+import org.junit.jupiter.api.Test;
+
 import br.com.koyama.apiqalab.base.BaseTest;
+import br.com.koyama.apiqalab.payloads.LoginPayload;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AuthTest extends BaseTest {
 
-    // TODO: implementar teste POST /login valido retornando 200
-    // TODO: validar token fake no campo data.token
-    // TODO: implementar teste POST /login invalido retornando 401
-    // TODO: validar mensagem clara de erro para credenciais invalidas
+	
+	@Test
+	public void deveRetornar200NoLoginValido() {		
+		given()
+		  .body(criarLoginValido())
+		.when()
+		  .post("/login")
+		.then()
+		  .statusCode(200)
+		;
+	}
+	
+	@Test
+	public void deveRetornarTokenNoLoginValido() {	    
+	    given()
+	      .body(criarLoginValido())
+	    .when()
+	      .post("/login")
+	    .then()
+	      .statusCode(200)
+	      .body("data.token", equalTo("fake-token-qa-lab-123456"))
+	    ;
+	}
+	
+	
+	@Test
+	public void deveRetornar401NoLoginInvalido() {    
+		given()
+		  .body(criarLoginInvalido())
+		.when()
+		  .post("/login")
+		.then()
+		  .statusCode(401) 
+		;
+	}
+	
+	
+	@Test
+	public void deveRetornarMensagemNoLoginInvalido() {	 
+		 given()
+		   .body(criarLoginInvalido())
+		 .when()
+		   .post("/login")
+		 .then()
+		   .statusCode(401)
+		   .body("message", equalTo("Usuário ou senha inválidos."))
+		 ;
+	}
+
+	
+	
+	private LoginPayload criarLoginValido() {
+      LoginPayload body = new LoginPayload();
+      body.setUsuario("admin");
+      body.setSenha("123456");
+      return body;
+	}
+	
+	private LoginPayload criarLoginInvalido() {
+      LoginPayload body = new LoginPayload();
+      body.setUsuario("admin");
+      body.setSenha("12345");
+      return body;
+	}
 }
--- automacao-rest-assured-api/src/test/java/br/com/koyama/apiqalab/tests/ClientesTest.java
+++ automacao-rest-assured-api/src/test/java/br/com/koyama/apiqalab/tests/ClientesTest.java
@@ -1,15 +1,136 @@
 package br.com.koyama.apiqalab.tests;
 
+import org.junit.jupiter.api.Test;
+
 import br.com.koyama.apiqalab.base.BaseTest;
+import br.com.koyama.apiqalab.payloads.ClientePayload;
+import br.com.koyama.apiqalab.utils.ResetUtils;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class ClientesTest extends BaseTest {
 
-    // TODO: implementar teste GET /clientes retornando 200
-    // TODO: implementar teste GET /clientes/{id} retornando 200 para cliente existente
-    // TODO: implementar teste GET /clientes/{id} retornando 404 para cliente inexistente
-    // TODO: implementar teste POST /clientes valido retornando 201
-    // TODO: implementar teste POST /clientes com e-mail invalido retornando 400
-    // TODO: implementar teste POST /clientes com CPF invalido retornando 400
-    // TODO: implementar teste PUT /clientes/{id} valido retornando 200
-    // TODO: implementar teste DELETE /clientes/{id} valido retornando 204
+	@Test
+	public void deveRetornar200AoBuscarClientes() {
+		given()
+		 .when()
+		  .get("/clientes")
+		 .then()
+		  .statusCode(200)
+		;
+	}
+
+	@Test
+	public void deveRetornar200AoBuscarClienteExistente() {
+		given()
+		 .when()
+		  .get("/clientes/1")
+		 .then()
+		  .statusCode(200)
+		 ;
+	}
+	
+	@Test
+	public void deveRetornar404ClienteInexistente() {
+		given()
+		 .when()
+		   .get("/clientes/999")
+		 .then()
+		   .statusCode(404)
+		;
+	}
+
+	@Test
+	public void deveRetornar201AoCadastrarClienteValido() {
+		given()
+		   .body(clienteValido())
+		 .when()
+		   .post("/clientes")
+		 .then()
+		   .statusCode(201)
+		;
+	}
+
+	@Test
+	public void deveRetornar400EmailInvalido() {
+		ClientePayload body = new ClientePayload();
+		body.setNome("Cliente Pay Lab");
+		body.setEmail("matheus");
+		body.setCpf("12345678900");
+		
+		given()
+          .body(body)
+         .when()
+          .post("/clientes")
+         .then()  
+          .statusCode(400)
+          .body("message", equalTo("E-mail inválido. Informe um endereço de e-mail válido."))
+		;
+	}
+	
+    @Test
+	public void deveRetornar400CPFInvalido() {
+		ClientePayload body = new ClientePayload();
+		body.setNome("Cliente Pay Lab");
+		body.setEmail("matheus@email.com");
+		body.setCpf("abc");
+		
+		given()
+		 .body(body)
+		.when()
+		 .post("/clientes")
+		.then()
+		 .statusCode(400)
+		 .body("message", equalTo("CPF inválido. Informe 11 dígitos numéricos."))
+		 ;
+	}
+
+    @Test
+	public void deveRetornar200AoAtualizarClienteValido() {
+
+	    ClientePayload body = new ClientePayload();
+	    body.setNome("Cliente Atualizado");
+	    body.setEmail("cliente.atualizado@qa.com");
+	    body.setCpf("52998224725");
+
+	    given()
+	        .body(body)
+	    .when()
+	        .put("/clientes/1")
+	    .then()
+	        .statusCode(200);
+	}
+
+    @Test
+	public void deveRetornar404AoDeletarClienteNaoEncontrado() {
+		given()
+		 .when()
+		   .delete("/clientes/4")
+		 .then()
+		   .statusCode(404)
+		   .body("message", equalTo("Cliente não encontrado."))
+		;
+	}
+    
+    @Test
+    public void deveRetornar204AoDeletarClienteValido() {
+      ResetUtils.resetarMassaDeDados();
+    	  given()
+    	   .when()
+    	     .delete("/clientes/1")
+    	   .then() 
+    	     .statusCode(204)
+    	  ;
+    }
+    
+
+	private ClientePayload clienteValido() {
+		ClientePayload body = new ClientePayload();
+		body.setNome("Cliente Pay Lab");
+		body.setEmail("matheus@email.com");
+		body.setCpf("12345678900");
+		return body;
+	}
+   
 }
