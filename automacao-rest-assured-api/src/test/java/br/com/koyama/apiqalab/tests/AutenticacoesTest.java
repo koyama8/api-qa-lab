5c5e24cabd91eab4a5a050e9688642843ff3d044
Matheus Koyama <koyamaCRS@gmail.com>
2026-05-11 19:55:51 -0300

    test: implementa testes de autenticacao publica, api key, basic e bearer

--- automacao-rest-assured-api/src/test/java/br/com/koyama/apiqalab/tests/AutenticacoesTest.java
+++ automacao-rest-assured-api/src/test/java/br/com/koyama/apiqalab/tests/AutenticacoesTest.java
@@ -2,27 +2,125 @@ package br.com.koyama.apiqalab.tests;
 
 import br.com.koyama.apiqalab.base.BaseTest;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
 public class AutenticacoesTest extends BaseTest {
 
-    // Topico 1.6 - Autenticacoes em API
 
     // TODO: implementar teste GET /auth/publica retornando 200 sem autenticacao.
+	@Test
+	public void deveRetornar200AoAcessarApiPublica() {
+		given()
+	    .when()	
+		    .get("/auth/publica")
+		.then()
+		    .statusCode(200)
+		    .body("message", equalTo("Operação realizada com sucesso"))
+		    .body("data.descricao", equalTo("Acesso publico sem autenticacao."))
+	 ;
+	}
 
     // TODO: implementar teste GET /auth/api-key retornando 200 com header x-api-key valido.
-
-    // TODO: implementar teste GET /auth/api-key retornando 401 sem header x-api-key.
-
-    // TODO: implementar teste GET /auth/api-key retornando 401 com API Key invalida.
+    @Test
+	public void deveRetornar200ComApiKeyValida() {
+	    given()
+	        .header("x-api-key", "qa-lab-api-key-123")
+	    .when() 
+	        .get("/auth/api-key")
+	    .then()
+	        .statusCode(200)
+	        .body("message", equalTo("Operação realizada com sucesso"))
+	        .body("data.tipo", equalTo("API_KEY"))
+	        .body("data.autenticado", equalTo(true))
+	        .body("data.api_key", equalTo("valida"));
+	}
+    
+    @Test
+    public void deveRetornar401APIAusente() {
+    	given()
+    	.when()
+    	    .get("/auth/api-key")
+    	.then()
+    	    .statusCode(401)
+    	    .body("message", equalTo("API Key ausente ou invalida."))
+    	;
+    }
+    
+    @Test
+    public void deveRetornar401APIKeyInvalida() {
+    	given()
+    	    .header("x-api-key", "123456")
+    	.when()
+    	    .get("/auth/api-key")
+    	.then()
+    	    .statusCode(401)
+    	    .body("message", equalTo("API Key ausente ou invalida."))
+    	;
+    }
+    
 
     // TODO: implementar teste GET /auth/basic retornando 200 com Basic Auth valido.
-
+    @Test
+    public void deveRetornar200ComBasicAuthValido() {
+    	given()
+            .auth().preemptive().basic("admin", "123456")
+    	.when()
+    	    .get("/auth/basic")
+    	.then()
+    	    .statusCode(200)
+    	;
+    }
+    
     // TODO: implementar teste GET /auth/basic retornando 401 com Basic Auth invalido.
-
+    @Test
+    public void deveRetornar401ComBasicAuthInvalido() {
+    	given()
+    	   .auth().preemptive().basic("admin", "123")
+    	.when()
+    	   .get("/auth/basic")
+    	.then() 
+    	   .statusCode(401)
+    	   .body("message", equalTo("Credenciais Basic Auth invalidas."))
+    	   
+    	;
+    }
     // TODO: implementar teste GET /auth/bearer retornando 200 com Bearer Token valido.
-
+    @Test
+    public void deveRetornar200ComBearerTokenValido() {
+    	given()
+    	   .auth().oauth2("fake-token-qa-lab-123456")
+    	.when() 
+    	   .get("/auth/bearer")
+    	.then()
+    	   .statusCode(200)
+    	   .body("message", equalTo("Operação realizada com sucesso"))
+    	   .body("data.tipo", equalTo("BEARER"))
+    	;
+    }
     // TODO: implementar teste GET /auth/bearer retornando 401 sem Bearer Token.
-
+    @Test
+    public void deveRetornar401ComBearerTokenAusente() {
+    	given()
+    	.when()
+    	    .get("/auth/bearer")
+    	.then()
+    	    .statusCode(401)
+    	    .body("message", equalTo("Bearer Token ausente ou invalido."))
+    	;
+    }
     // TODO: implementar teste GET /auth/bearer retornando 401 com Bearer Token invalido.
-
-    // TODO: aplicar autenticacao nos testes usando header(), auth().basic() e bearerToken().
+    @Test
+    public void deveRetornar401ComBearerTokenInvalido() {
+    	given()
+    	    .auth().oauth2("fake-token-qa-lab")
+    	.when() 
+    	    .get("/auth/bearer")
+    	.then() 
+    	    .statusCode(401)
+    	    .body("message", equalTo("Bearer Token ausente ou invalido."))
+    	;
+    }
 }
